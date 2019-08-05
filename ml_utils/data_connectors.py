bffd7d87c03ec015095ad31327cd12c275bf1499
Praveen <praveengovi@gmail.com>
2019-08-05 22:59:54 +0800

    Adding modules resumetable,reduce_mem_usage,CalcOutliers

---
+++ ml_utils/data_connectors.py
@@ -0,0 +1,7 @@
+## Import data from different data sources
+## Flat files - Excel , txt , csv & multitab excel
+## Different databases - mysql , mariadb , Oracle , SQL Server , Teradata  .etc
+## cloud services - AWS s3 , Azure Blob & Google bigtable
+## Bigdata ecosystem - HDFS , hive
+## NoSQL - Mongodb,cassandra,Hbase,Neo4J,graphQL
+## Elastic search
